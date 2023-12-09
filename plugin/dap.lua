361c4cb2af20e3c2c021ae2e96d764eccc0d9af4
vinayak <itssvinayak@gmail.com>
2023-12-09 19:40:52 +0530

    git ignore and bug fix

--- plugin/dap.lua
+++ plugin/dap.lua
@@ -56,10 +56,31 @@ local function configure_debuggers()
 end
 
 function dapConfigMap.setup()
-    configure()                    -- Configuration
-    configure_exts()               -- Extensions
-    configure_debuggers()          -- Debugger
+    configure()                           -- Configuration
+    configure_exts()                      -- Extensions
+    configure_debuggers()                 -- Debugger
     require("plugin.dap.keymaps").setup() -- Keymaps
 end
 
-dapConfigMap.setup()
+-- dapConfigMap.setup()
+--
+--
+--
+
+
+
+require("dapui").setup()
+
+local dap, dapui = require("dap"), require("dapui")
+
+dap.listeners.after.event_initialized["dapui_config"] = function()
+    dapui.open({})
+end
+dap.listeners.before.event_terminated["dapui_config"] = function()
+    dapui.close({})
+end
+dap.listeners.before.event_exited["dapui_config"] = function()
+    dapui.close({})
+end
+
+vim.keymap.set('n', '<leader>ui', require 'dapui'.toggle)
